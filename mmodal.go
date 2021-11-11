9f400062dbc615afed4b11e9eb2f3fdeb037d582
James LaPointe <james.lapointe@accesscfa.com>
2021-11-11 12:01:18 -0800

    updates

--- mmodal.go
+++ mmodal.go
@@ -10,7 +10,7 @@ import (
 )
 
 // create function that takes int and slice and returns a slice
-func CumulativeAverages(inputSlice []float64, averageWindow int) []float64 {
+func CumulativeAverages(inputSlice []float64, intWindow int) []float64 {
 	// slice to hold hold avg
 	avg := make([]float64, len(inputSlice))
 	runningSum := inputSlice[0]
@@ -18,16 +18,14 @@ func CumulativeAverages(inputSlice []float64, averageWindow int) []float64 {
 	avg[0] = inputSlice[0] / float64(divisidor)
 
 	for j := 1; j < len(inputSlice); j++ {
-		if j < averageWindow {
+		if j < intWindow {
 			runningSum += inputSlice[j]
 			divisidor = divisidor + 1
 		} else {
-			runningSum -= inputSlice[j-averageWindow]
+			runningSum -= inputSlice[j-intWindow]
 			runningSum += inputSlice[j]
-			divisidor = averageWindow
+			divisidor = intWindow
 		}
-		fmt.Println(runningSum)
-		fmt.Println(float64(divisidor))
 		avg[j] = runningSum / float64(divisidor)
 	}
 	return avg
@@ -36,13 +34,8 @@ func CumulativeAverages(inputSlice []float64, averageWindow int) []float64 {
 
 func main() {
 
-	// create a slice with values
-	//slice := []float64{0, 1, 2, 3}
-
-	// slice2 := []float64{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}
-	// fmt.Println(CumulativeAverages(slice, 3))
-	// fmt.Println(CumulativeAverages(slice2, 5))
-
+	// In main - take one argument queoted - with numbers - as descibed in assignment
+	// like ... #go run mmodal.go "(3, [0, 1, 2, 3])"
 	// Take command line args - and remove non numerics besides comma and periods
 	reg, err := regexp.Compile("[^,0-9,.]+")
 	if err != nil {
@@ -53,26 +46,24 @@ func main() {
 
 	// Create slice from string - split on comma
 	slice3 := strings.Split(processedString, ",")
-	length := len(slice3) - 1
-	// declare slice of length 
 	processSlice := make([]float64, len(slice3)-1)
-	 := []float64{}
-	sumAverage, err := strconv.Atoi(slice3[0])
+
+	// get integer window
+	intWindow, err := strconv.Atoi(slice3[0])
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(sumAverage)
+
 	for i := 1; i < len(slice3); i++ {
-		fmt.Println(slice3[i])
 		processSlice[i-1], err = strconv.ParseFloat(slice3[i], 64)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		processSlice = append(processSlice, res)
+		//processSlice = append(processSlice, res)
 
 	}
 
 	// TODO - split the string - with , and convert to fload slice
-	//fmt.Println(CumulativeAverages(processSlice, sumAverage))
+	fmt.Println(CumulativeAverages(processSlice, intWindow))
 }
--- mmodal_test.go
+++ mmodal_test.go
@@ -6,40 +6,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type Resource struct {
-	//ID          string `json:"id" bson:"_id,omitempty"`
-	window int32
-	values []float64
-}
+func TestMModal1(t *testing.T) {
 
-// Create a resource with a window of 3
+	slc := []float64{0, 1, 2, 3}
+	//slice2 := []float64{0, 1, -2, 3, -4 5, -6, 7, -8, 9}
 
-// var struc = []Resource{
-// 	{3, {1, 2, 3, 4}},
-// 	{4, {5, 6, 7, 8}},
-// }
-
-func TestMModal(t *testing.T) {
-
-	slice := []float64{0, 1, 2, 3}
-	//slice2 := []float64{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}
-
-	sliceRes1 := CumulativeAverages(slice, 3)
+	sliceRes := CumulativeAverages(slc, 3)
 	sliceExp := []float64{0, 0.5, 1, 2}
-	// assert that sliceRes1 is equal to sliceRes2
-	assert.EqualValues(t, sliceRes1, sliceExp)
+	// assert that sliceRes is equal to sliceRes
+	assert.EqualValues(t, sliceRes, sliceExp)
 
 }
 
 func TestMModal2(t *testing.T) {
 
-	slice := []float64{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}
+	slc := []float64{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}
 
-	//slice2 := []float64{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}
-
-	sliceRes1 := CumulativeAverages(slice, 5)
+	sliceRes := CumulativeAverages(slc, 5)
 	sliceExp := []float64{0, 0.5, -0.3333333333333333, 0.5, -0.4, 0.6, -0.8, 1, -1.2, 1.4}
-	// assert that sliceRes1 is equal to sliceRes2
-	assert.EqualValues(t, sliceRes1, sliceExp)
+	// assert that sliceRes is equal to sliceRes
+	assert.EqualValues(t, sliceRes, sliceExp)
+
+}
+
+func TestMModal3(t *testing.T) {
+
+	slc := []float64{1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100}
+
+	sliceRes := CumulativeAverages(slc, 5)
+	sliceExp := []float64{1, 3, 5.333333333333333, 7.75, 10.2, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90}
+	// assert that sliceRes is equal to sliceRes
+	assert.EqualValues(t, sliceRes, sliceExp)
 
 }
