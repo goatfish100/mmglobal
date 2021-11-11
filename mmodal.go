b467fd095e390309baa805ea05b5edab83e6de51
James LaPointe <james.lapointe@accesscfa.com>
2021-11-10 22:39:51 -0800

    updates

--- mmodal.go
+++ mmodal.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 // create function that takes int and slice and returns a slice
 func CumulativeAverages(inputSlice []float64, averageWindow int) []float64 {
@@ -28,11 +35,44 @@ func CumulativeAverages(inputSlice []float64, averageWindow int) []float64 {
 }
 
 func main() {
-	fmt.Println("vim-go")
-	// create a slice with values
-	slice := []float64{0, 1, 2, 3}
-	slice2 := []float64{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}
-	fmt.Println(CumulativeAverages(slice, 3))
-	fmt.Println(CumulativeAverages(slice2, 5))
 
+	// create a slice with values
+	//slice := []float64{0, 1, 2, 3}
+
+	// slice2 := []float64{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}
+	// fmt.Println(CumulativeAverages(slice, 3))
+	// fmt.Println(CumulativeAverages(slice2, 5))
+
+	// Take command line args - and remove non numerics besides comma and periods
+	reg, err := regexp.Compile("[^,0-9,.]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	processedString := reg.ReplaceAllString(os.Args[1], "")
+
+	// Create slice from string - split on comma
+	slice3 := strings.Split(processedString, ",")
+	length := len(slice3) - 1
+	// declare slice of length 
+	processSlice := make([]float64, len(slice3)-1)
+	 := []float64{}
+	sumAverage, err := strconv.Atoi(slice3[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(sumAverage)
+	for i := 1; i < len(slice3); i++ {
+		fmt.Println(slice3[i])
+		processSlice[i-1], err = strconv.ParseFloat(slice3[i], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		processSlice = append(processSlice, res)
+
+	}
+
+	// TODO - split the string - with , and convert to fload slice
+	//fmt.Println(CumulativeAverages(processSlice, sumAverage))
 }
--- mmodal_test.go
+++ mmodal_test.go
@@ -2,7 +2,22 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+type Resource struct {
+	//ID          string `json:"id" bson:"_id,omitempty"`
+	window int32
+	values []float64
+}
+
+// Create a resource with a window of 3
+
+// var struc = []Resource{
+// 	{3, {1, 2, 3, 4}},
+// 	{4, {5, 6, 7, 8}},
+// }
 
 func TestMModal(t *testing.T) {
 
@@ -10,5 +25,21 @@ func TestMModal(t *testing.T) {
 	//slice2 := []float64{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}
 
 	sliceRes1 := CumulativeAverages(slice, 3)
-	t.Log(sliceRes1)
+	sliceExp := []float64{0, 0.5, 1, 2}
+	// assert that sliceRes1 is equal to sliceRes2
+	assert.EqualValues(t, sliceRes1, sliceExp)
+
+}
+
+func TestMModal2(t *testing.T) {
+
+	slice := []float64{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}
+
+	//slice2 := []float64{0, 1, -2, 3, -4, 5, -6, 7, -8, 9}
+
+	sliceRes1 := CumulativeAverages(slice, 5)
+	sliceExp := []float64{0, 0.5, -0.3333333333333333, 0.5, -0.4, 0.6, -0.8, 1, -1.2, 1.4}
+	// assert that sliceRes1 is equal to sliceRes2
+	assert.EqualValues(t, sliceRes1, sliceExp)
+
 }
